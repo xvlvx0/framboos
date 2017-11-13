d0688c46b25d15a9a22e449b8744890d5e59b174
xvlvx0 <xvlvx0@gmail.com>
2017-11-13 13:31:44 +0100

    LED's

--- test_pin.py
+++ test_pin.py
@@ -4,8 +4,9 @@ import time
 #from GPIOEmulator.EmulatorGUI import GPIO	# imports the GPIO emulator, for testing only
 import RPi.GPIO as GPIO		# imports the Rasp GPIO module (General Purpose Input/Output)
 
-pinnr1 = 21			# GPIO pin number
-pinnr2 = 26			# GPIO pin number
+pinnr1 = 19			# GPIO pin number
+pinnr2 = 21			# GPIO pin number
+pinnr3 = 26			# GPIO pin number
 
 def setupGPIO():
 	print ("Setting up the GPIO pins\n")
@@ -15,32 +16,25 @@ def setupGPIO():
 	GPIO.setwarnings(False)
 
 	#GPIO.setup(pinnr, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-	GPIO.setup(pinnr1, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-	GPIO.setup(pinnr2, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+	#GPIO.setup(pinnr1, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+	GPIO.setup(pinnr1, GPIO.IN)
+	GPIO.setup(pinnr2, GPIO.IN)
+	GPIO.setup(pinnr3, GPIO.IN)
 
 def readPin(pin):
 	value = GPIO.input(pin)
 	return value
 
 setupGPIO()
-i = 100
-while(i>1):
-	print("reading state:....")
-	
+print("reading state:....")
+while(1):
 	state1 = readPin(pinnr1)
-	if state1 == True:
-		print("Pin1 is HIGH\n")
-	else:
-		print("Pin1 is LOW\n")
-
 	state2 = readPin(pinnr2)
-	if state2 == True:
-		print("Pin2 is HIGH\n")
-	else:
-		print("Pin2 is LOW\n")
+	state3 = readPin(pinnr3)
+
+	print("Pin1 is %,\tPin2 is %,\tPin3 is %.\n", str(state1), str(state2), str(state3))
 	
 	time.sleep(1)
-	i -= 1
 
 print ("cleanup pins")
 GPIO.cleanup() #this ensures a clean exit
