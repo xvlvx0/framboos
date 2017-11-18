d3a6e4e5a7dcfd2e32b480fc7a45272b87180973
root <xvlvx0@gmail.com>
2017-11-18 16:46:30 +0100

    cleanup

--- gpio/test_pin.py
+++ gpio/test_pin.py
@@ -7,19 +7,21 @@ import RPi.GPIO as GPIO		# imports the Rasp GPIO module (General Purpose Input/O
 pinnr1 = 19			# GPIO pin number
 pinnr2 = 21			# GPIO pin number
 pinnr3 = 26			# GPIO pin number
+pins = {pinnr1, pinnr2, pinnr3}
 
-def setupGPIO():
+def setupGPIO_IN(pins):
 	print ("Setting up the GPIO pins\n")
 	#GPIO.setmode(GPIO.BOARD) 		# sets input to unified board pin numbers
 	GPIO.setmode(GPIO.BCM)			# sets input to chip numbers, can differ per poard type
-
 	GPIO.setwarnings(False)
+
+	for pin in pins:
+		GPIO.setup(pin, GPIO.IN)
 
 	#GPIO.setup(pinnr1, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 	#GPIO.setup(pinnr2, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-	GPIO.setup(pinnr1, GPIO.IN)
-	GPIO.setup(pinnr2, GPIO.IN)
-	GPIO.setup(pinnr3, GPIO.IN)
+	#GPIO.setup(pinnr1, GPIO.IN)
+	#GPIO.setup(pinnr2, GPIO.OUT)
 
 def readPin(pin):
 	value = GPIO.input(pin)
@@ -29,14 +31,11 @@ setupGPIO()
 print("reading state:....")
 counter = 0
 while(counter < 120):
-	state1 = readPin(pinnr1)
-	state2 = readPin(pinnr2)
-	state3 = readPin(pinnr3)
-
-	print(str(counter) + " - " + str(state1) + " - " + str(state2) + " - " + str(state3) + ".\n")
+	for pin in pins:
+		state = readPin(pinn)
+		print(str(counter) + " - " + str(state1))
 	
 	time.sleep(0.5)
-
 	counter += 1
 
 print ("cleanup pins")
